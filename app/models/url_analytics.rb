8beda77b721c9c818126613c75b81a460e9c75f3
PenneyGadget <dreamofgreen@gmail.com>
2015-12-09 15:29:41 -0700

    2 passing url stat tests

--- app/models/url_analytics.rb
+++ app/models/url_analytics.rb
@@ -2,7 +2,17 @@ module TrafficSpy
   class UrlAnalytics
 
     def longest_response_time(identifier)
-      u = Users.find_by(identifier: identifier)
+      u = TrafficSpy::User.find_by(identifier: identifier)
+      response_time = u.payloads.map { |p| p.responded_in }
+      response_time.sort.last
+    end
+
+    def shortest_response_time
+
+    end
+
+    def average_response_time
+
     end
 
   end
--- test/models/url_analytics_test.rb
+++ test/models/url_analytics_test.rb
@@ -3,13 +3,13 @@ require_relative '../test_helper'
 class UrlAnalyticsTest < ModelTest
 
   def load_user_info(n)
-    TrafficSpy::User.create("identifier"=>"identifier#{n}", "root_url"=>"http://root_url#{n}.com")
+    TrafficSpy::User.find_or_create_by("identifier"=>"identifier#{n}", "root_url"=>"http://root_url#{n}.com")
   end
 
-  def load_user_payload(n, verb = "GET", datetime = "2013-02-16 21:38:28 -0700")
+  def load_user_payload(n, verb = "GET", response_time = 37)
     TrafficSpy::Payload.create({"url"=>"url#{n}",
-                                "requested_at"=>datetime,
-                                "responded_in"=>37,
+                                "requested_at"=>"2013-02-16 21:38:28 -0700",
+                                "responded_in"=>response_time,
                                 "referred_by"=>"http://root_url#{n}.com",
                                 "request_type"=>verb,
                                 "event_name"=>"event_name#{n}",
@@ -20,8 +20,8 @@ class UrlAnalyticsTest < ModelTest
                                 "platform"=>"platform#{n}"})
   end
 
-  def associate_user_payload(n, verb = "GET", datetime = "2013-02-16 21:38:28 -0700")
-    load_user_info(n).payloads << load_user_payload(n, verb, datetime)
+  def associate_user_payload(n, verb = "GET", response_time = 37)
+    load_user_info(n).payloads << load_user_payload(n, verb, response_time)
   end
 
   def test_we_can_access_a_specfic_users_payloads
@@ -30,17 +30,45 @@ class UrlAnalyticsTest < ModelTest
 
     u1 = TrafficSpy::User.find_by(identifier: "identifier1")
     payload_1 = u1.payloads
-    ip = payload_1.map { |p| p.ip }
+    ip = payload_1.first.ip
 
-    assert payload_1
-    assert_equal ["63.29.38.211"], ip
+    assert_equal "63.29.38.211", ip
 
     u2 = TrafficSpy::User.find_by(identifier: "identifier1")
     payload_2 = u2.payloads
-    verb = payload_2.map { |p| p.request_type}
+    verb = payload_2.last.request_type
 
-    assert payload_2
-    assert_equal ["GET"], verb
+    assert_equal "GET", verb
+  end
+
+  def test_longest_response_time
+    associate_user_payload(1)
+    associate_user_payload(1, "GET", 60)
+    associate_user_payload(1, "GET", 13)
+
+    u = TrafficSpy::UrlAnalytics.new
+
+    assert_equal 60, u.longest_response_time("identifier1")
+  end
+
+  def test_shortest_response_time
+
+  end
+
+  def test_average_response_time
+
+  end
+
+  def test_we_can_collect_http_verbs_that_have_been_used
+
+  end
+
+  def test_most_popular_referrers
+
+  end
+
+  def test_most_popular_user_agents
+
   end
 
 end
