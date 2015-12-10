a5276a1aee8c057cfe8ac176dc2d95846dfd51cd
Aaron Greenspan <afg419@gmail.com>
2015-12-10 16:48:31 -0700

    all but app tests working

--- test/features/event_index_test.rb
+++ test/features/event_index_test.rb
@@ -14,7 +14,8 @@ class EventIndexTest < FeatureTest
                "ip"=>"63.29.38.211",
                "user_id"=>1,
                "browser"=>"Mozilla",
-               "platform"=>"Mac"}
+               "platform"=>"Mac",
+              "payload_sha"=>"09138409813409834"}
   end
 
   def test_the_truth
@@ -23,7 +24,7 @@ class EventIndexTest < FeatureTest
 
   def test_goes_to_event_index_page
     TrafficSpy::User.create("identifier" => "jumpstartlab", "root_url" => "http://jumpstartlab.com")
-    TrafficSpy::Payload.create(payload)
+    TrafficSpy::DbLoader.new(payload,"jumpstartlab").load_databases
 
     visit('/sources/jumpstartlab/events')
 
