c207a641d7cb436e3b34734bc1d2f2366aee5092
Aaron Greenspan <afg419@gmail.com>
2015-12-10 16:18:36 -0700

    test: url analytics done

--- app/models/url_analytics.rb
+++ app/models/url_analytics.rb
@@ -7,33 +7,33 @@ module TrafficSpy
 
     def longest_response_time(identifier)
       client = find_client(identifier)
-      client.payloads.maximum(:responded_in)
+      client.urls.maximum(:responded_in)
     end
 
     def shortest_response_time(identifier)
       client = find_client(identifier)
-      client.payloads.minimum(:responded_in)
+      client.urls.minimum(:responded_in)
     end
 
     def average_response_time(identifier)
       client = find_client(identifier)
-      client.payloads.average(:responded_in).round(3).to_f
+      client.urls.average(:responded_in).round(3).to_f
     end
 
     def verbs_used(identifier)
       client = find_client(identifier)
-      client.payloads.pluck(:request_type)
+      client.urls.pluck(:request_type)
     end
 
     def most_popular_referrers(identifier)
       client = find_client(identifier)
-      referrers = client.payloads.group(:referred_by).count
+      referrers = client.urls.group(:referred_by).count
       referrers.sort_by { |k, v| [-v, k] }.map{ |u| u[0] }[0..2]
     end
 
     def most_popular_user_agents(identifier) #browser
       client = find_client(identifier)
-      browsers = client.payloads.group(:browser).count
+      browsers = client.urls.group(:browser).count
       browsers.sort_by { |k, v| [-v, k] }.map{ |u| u[0] }[0..2]
     end
 
